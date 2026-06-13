c0fab7cb01bfd06b5989cca54556f50e022ae906
minedcaesar <fanara.gaspare@gmail.com>
2026-06-13 17:10:13 +0200

    db

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
+from database.db import init_app
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    init_app(app)
     return app
 
 
