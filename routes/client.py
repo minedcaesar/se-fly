fc240bed7619f9b7fb7a5b3215e6965588835151
cloudingvi <giorgia.conte@studenti.unitn.it>
2026-06-14 11:57:45 +0200

    flight board

--- routes/client.py
+++ routes/client.py
@@ -1,6 +1,6 @@
 # routes facing the clients (dashboard, airport info)
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 from database.db import get_db
 from routes import login_required
@@ -21,3 +21,20 @@ def info():
     rows = db.execute('SELECT key, content, content_type FROM airport_content').fetchall()
     content = {row['key']: row for row in rows}
     return render_template('client/info.html', content=content)
+
+
+@bp.route('/flights')
+def flights():
+    db = get_db()
+    iata = current_app.config['AIRPORT_IATA_CODE']
+    departures = db.execute(
+        'SELECT fi.*, fs.flight_number, fs.origin, fs.destination '
+        'FROM flight_instances fi JOIN flight_schedules fs ON fi.schedule_id = fs.id '
+        'WHERE fs.origin = ? ORDER BY fi.scheduled_departure_time', (iata,),
+    ).fetchall()
+    arrivals = db.execute(
+        'SELECT fi.*, fs.flight_number, fs.origin, fs.destination '
+        'FROM flight_instances fi JOIN flight_schedules fs ON fi.schedule_id = fs.id '
+        'WHERE fs.destination = ? ORDER BY fi.scheduled_arrival_time', (iata,),
+    ).fetchall()
+    return render_template('client/flights.html', departures=departures, arrivals=arrivals)
